b7cfc17ae74c863cfd46b6ba390f32452750a78f
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-07-08 13:12:42 -0400

    Minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bertalign',
+    version='0.1.0',
+    url='https://github.com/bfsujason/bertalign',
+    description='An automatic mulitlingual sentence aligner.',
+    packages=find_packages(),    
+    install_requires=[],
+)
